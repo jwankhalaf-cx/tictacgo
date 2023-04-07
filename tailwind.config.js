8772c79e41bf8c0d76ff1168e87d8e4eda3b62a5
Rawan Hamza <70547783+Rawan-Hamza@users.noreply.github.com>
2023-04-07 16:58:04 +0100

    feat/12-set-up-a-new-blazor-project (#1)
    * create blazor app with tailwind css
    
    * project-setup with tailwind css
    
    * project setup with tailwind css
    
    * remove unnecessary files
    
    * add targets to ensure node installation and minify css
    
    * fix tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '!**/{bin,obj,node_modules}/**',
+    '**/*.{razor,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
