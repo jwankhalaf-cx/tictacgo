1fa40e18f98443f64cae38443bb8d89d56989eda
jwankhalaf-cx <123021496+jwankhalaf-cx@users.noreply.github.com>
2023-04-15 11:46:46 +0100

    feat/4-add-tailwindcss-to-ui-project (#4)
    * add and configure tailwind css
    
    * format xml
    
    * check for node version instead of npm
    
    * add npm last install property
    
    ---------
    
    Co-authored-by: Jwan Khalaf <jwan.khalaf@outlook.com>

---
+++ UI/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '!**/{bin,obj,node_modules}/**',
+    '**/*.{razor,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')]
+}
+
